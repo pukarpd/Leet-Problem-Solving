c45e4e9e382e46861ed235c17fa8659be17f3b66
pukarpd <107954286+pukarpd@users.noreply.github.com>
2025-12-05 16:17:12 -0500

    16 ms (50.13%) 37.5 MB (64.75%) 12-05-2025 0334-increasing-triplet-subsequence Medium .py

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -7,19 +7,35 @@ class Solution:
         if len(nums) == 3: 
             return True if nums[0] < nums[1] and nums[1] < nums[2] else False
         
-        i, j, k = 0, 1, 2
+        first = float('inf')
+        second = float('inf')
 
-        while i < len(nums) - 2: 
-            if nums[i] < nums[j] and nums[j] < nums[k]: 
-                return True
-            elif nums[i] > nums[j]: 
-                i += 1 
-                j = i + 1 
-                k = j + 1 
-            elif nums[i] < nums[j] and nums[j] > nums[k]: 
-                while nums[j] > nums[k]: 
-                    k += 1 
-        
+        for num in nums: 
+            if num <= first:
+                first = num
+                continue 
+            if num <= second: 
+                second = num
+                continue 
+            if num > first and num > second:
+                return True 
         return False
+
+        # #flawed attempt
+        # while i < len(nums) - 2 and k < len(nums): 
+        #     print(i,j,k)
+        #     if nums[i] < nums[j] and nums[j] < nums[k]: 
+        #         return True
+        #     elif nums[i] > nums[j]: 
+        #         i += 1 
+        #         j = i + 1 
+        #         k = j + 1 
+        #     if nums[i] < nums[j]:
+        #         while nums[j] > nums[k] and k < len(nums): 
+        #             print(j,k)
+        #             k += 1 
+
+        
+        # return False
         
 
