0d715885ed23b255648cffc00eac0ec87b987fbe
pukarpd <107954286+pukarpd@users.noreply.github.com>
2026-02-11 16:18:39 -0500

    0 ms (100%) 20.1 MB (17.37%) 02-11-2026 0102-binary-tree-level-order-traversal Medium .py

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+        def bfs(root): 
+            if not root: 
+                return []
+        
+            dq = deque([root])
+            res = []
+            while dq: 
+                lvl = [] 
+                for _ in range(len(dq)): 
+                    node = dq.popleft()
+                    lvl.append(node.val)
+                    if node.left: 
+                        dq.append(node.left)
+                    if node.right: 
+                        dq.append(node.right)
+                res.append(lvl)
+
+            return res 
+        return bfs(root)
+
+
+            
