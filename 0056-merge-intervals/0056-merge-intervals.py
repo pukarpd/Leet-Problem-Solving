35c2c0bededc425b96491a6206e8933d33b41d43
pukarpd <107954286+pukarpd@users.noreply.github.com>
2026-02-16 13:39:20 -0500

    7 ms (71.94%) 22.7 MB (43.88%) 02-16-2026 0056-merge-intervals Medium .py

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,92 +1,19 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # O(nlogn)
-        # sort by start value
+    
+        intervals.sort(key = lambda i : i[0], reverse = True)
 
-        intervals.sort(key = lambda i: i[0])
-        output = [intervals[0]]
         # print(intervals)
-        for start, end in intervals[1:]: 
-            # print(output, start, end)
-            lastEnd = output[-1][1]
-            # print(lastEnd)
-            if start <= lastEnd: 
-                output[-1][1] = max(lastEnd, end)
+        res = [] 
+        res.append(intervals.pop())
+        for i in range(len(intervals)-1,-1,-1):
+            temp = intervals[i] 
+            # print(res, temp)
+            if res[-1][1] >= temp[0]: 
+                res[-1][1] = max(temp[1], res[-1][1])
             else: 
-                output.append([start, end])
-            # [1,5] , [2,4] we want to keep the 5m thats why the last end vs end merging. 
-        return output
-        # # my method
-        # new = [] 
-        # for i in intervals: 
-        #     new.append(i[0])
-        # zipped_intervals = sorted(zip(intervals, new))
-
-        # new_intervals = [item[0] for item in zipped_intervals] 
-        # print(new_intervals)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # sorter = []
-        # for arr in intervals: 
-        #     suma = sum(arr)
-        #     sorter.append(suma)
+                res.append(temp)
         
-        # news = sorted(zip(sorter, intervals))
-        # new = [item[1] for item in news]
-        
+        return res[::-1]
 
-        # left = 0 
-        # right = 1
-        # res = []
-        # for i in range(len(new)):
-        #     while right < ROWS: 
-        #         num1 = new[left][1]
-        #         num2 = new[right][0]
-        #         if num1 >= num2:
-        #             newarr = [item for item in new[left]]
-        #             for item in new[right]: 
-        #                 newarr.append(item)
-        #             print(newarr)
-        #             res.append([min(newarr), max(newarr)])
-        #             left += 2 
-        #             right += 2
-        #         else: 
-        #             res.append(new[left])
-        #             left += 1 
-        #             right += 1 
-        #         new = res
-        #         left = 0 
-        #         right = 1
-
-        # if right == len(new): 
-        #     res.append(new[right -1 ])
-        # print(new, res)
-
-        # return res
-        
-
-
-
+        # i, j = 0 , 1: 
