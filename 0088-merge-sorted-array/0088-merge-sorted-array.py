11ca8bf8c996ce4459eaac410d134ae345a9efca
pukarpd <107954286+pukarpd@users.noreply.github.com>
2026-02-18 12:25:53 -0500

    N/A (0%) N/A (0%) 02-18-2026 0088-merge-sorted-array Easy .py

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,11 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m 
+
+        i = 0 
+        j = 0 
+
+        smallerVal = min(m, n)
+        while i <  smallerVal and j < smallerVal:
+            if nums1[i] > nums2[j]: 
+                nums1[i], nums2[j] = nums2[j], nums1[i]
+                i += 1
+                j += 1 
+            else: 
+                i += 1 
         rest = 0 
-        while i < n+m: 
+        # if i == m: 
+        while rest < n: 
             nums1[i] = nums2[rest]
-            i+=1
-            rest+=1
-        print(nums1)
-        nums1.sort()
+            i += 1 
+            rest += 1 
+
+
