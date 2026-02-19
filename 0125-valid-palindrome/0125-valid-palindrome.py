9ce71cb41c765502903cce68b2c6ca0199f89894
pukarpd <107954286+pukarpd@users.noreply.github.com>
2026-02-18 19:03:00 -0500

    11 ms (35.05%) 19.7 MB (46.31%) 02-18-2026 0125-valid-palindrome Easy .py

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,38 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        if len(s) == 1: 
-            return True 
-
-      
-
-
-        newstring = ""
-
+        comp = ''
         for i in range(len(s)): 
             if s[i].isalnum():
-                newstring += s[i].lower()
+                comp += s[i].lower()
         
-        if len(newstring) == 0: 
-            return True
-                
+        l, r = 0, len(comp) - 1 
 
-        i = 0
-        j = len(newstring) - 1  
-      
-        while  i < (len(newstring) - 1) / 2 : 
-            if newstring[i] != newstring[j]:
-                return False
-            i += 1 
-            j -= 1 
-        
-        print(i)
-        print(j)
-
-        mybooleanCheck = newstring[i] == newstring[j]
-       
-    
-        return mybooleanCheck
-
-
-        
+        while l <= r: 
+            # print(l,r)
+            if comp[l] != comp[r]: 
+                return False 
+            l += 1 
+            r -= 1 
+        return True
