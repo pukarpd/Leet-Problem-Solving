de3413ab432b993f61e002f8242a2142b428fb93
pukarpd <107954286+pukarpd@users.noreply.github.com>
2026-03-11 16:44:44 -0400

    42 ms (80.58%) 19.1 MB (91.78%) 03-11-2026 0190-reverse-bits Easy .py

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,6 +1,10 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        
-        padding = ''.join(['0'] * (32 - len(bin(n)[2:])))
-        new_bin = (padding + (bin(n)[2:]))
-        return int(new_bin[::-1], 2)
+        res =  0
+        for i in range(32): 
+            bit = (n>>i) & 1            # we take the last bit
+            res = res | (bit << (31 - i))
+        return res 
+        # padding = ''.join(['0'] * (32 - len(bin(n)[2:])))
+        # new_bin = (padding + (bin(n)[2:]))
+        # return int(new_bin[::-1], 2)
