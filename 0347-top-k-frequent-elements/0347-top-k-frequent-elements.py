dcd086d832833ae38355c5dc718c79a196c30a72
pukarpd <107954286+pukarpd@users.noreply.github.com>
2026-03-18 22:48:47 -0400

    147 ms (5%) 22 MB (100%) 03-18-2026 0347-top-k-frequent-elements Medium .py

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        count = {} 
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums: 
+            count [n] = 1 + count.get(n, 0) #populates a hashmap 
+        for key, value in count.items(): 
+            freq[value].append(key)
+        
+        result = [] 
+
+        for i in range(len(freq) - 1, 0, -1): 
+            for n in freq[i]: 
+                result.append(n)
+                if len(result) == k: 
+                    return result 
+        
+
