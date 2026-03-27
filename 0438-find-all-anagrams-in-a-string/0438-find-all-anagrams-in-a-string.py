f3e2a52090d7dc7156fd606f2736a1fdb1df0fe1
pukarpd <107954286+pukarpd@users.noreply.github.com>
2026-03-27 19:54:53 -0400

    5460 ms (10.54%) 19.9 MB (31.95%) 03-27-2026 0438-find-all-anagrams-in-a-string Medium .py

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,38 +1,30 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         if len(p) > len(s): 
-            return []
+            return [] 
 
-        p = sorted(p)
-        print(p)
         left, right = 0, len(p) - 1 
-        res = []
-
+        res = [] 
+        
+        newP = Counter(p)
         while right < len(s): 
-            curString = s[left:right+1]
-            sortedString = sorted(curString)
-            # print(f"sortedstring:{sortedString}")
-            if sortedString == p: 
+            mySet = Counter(s[left:right+1])
+            # print(mySet, newP)
+            if mySet == newP: 
                 res.append(left)
             left += 1 
-            right += 1 
-
+            right += 1
+        
         return res
-            
 
-    # def quicksort(self, s): 
-    #     s = list(s)
-    #     if len(s) <= 1: 
-    #         return s 
-        
-    #     pivot = s[-1]
-    #     index = 0
-    #     for i in range(len(s)):
-    #         if ord(pivot) < s[i]: 
-    #             s[i], pivot = pivot, s[i]
-    #             index = i
-        
-    #     return self.quicksort(s[:index+1]) + self.quicksort(s[index:])
+    def quicksort(self, s): 
+        s = list(s)
+        if len(s) <= 1: 
+            return s 
+        p = s[-1]
+        left = [x for x in s[:-1] if x <= p]
+        right = [x for x in s[:-1] if x > p]
+        return self.quicksort(left) + [p] + self.quicksort(right)
 
 
 
