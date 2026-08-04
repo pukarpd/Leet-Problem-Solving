9b993a001e2d746cae61a9885afa4f00bfebf882
pukarpd <107954286+pukarpd@users.noreply.github.com>
2026-08-03 22:26:13 -0400

    Time: 0 ms (100%), Space: 19 MB (99.13%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        # alice, bob = 0, 0 
+
+        # i, j = 0, len(piles) - 1
+
+        # while i < j: 
+        #     if piles[i] > piles[j]: 
+
+        return True
