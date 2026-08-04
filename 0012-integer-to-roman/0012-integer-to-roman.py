f7afa1992bb1d529dfe7a84da30ebdad071ccf4d
pukarpd <107954286+pukarpd@users.noreply.github.com>
2026-08-03 23:13:22 -0400

    Time: 23 ms (5.24%), Space: 19.2 MB (63.3%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,19 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        itr = "1 4 5 9 10 40 50 90 100 400 500 900 1000"
+        rti = "I IV V IX X XL L XC C CD D CM M"
+
+        itr = itr.split(" ")
+        itr = [int(i) for i in itr]
+
+        rti = rti.split(" ")
+        map = {itr[i]: rti[i] for i in range(len(itr)-1,-1,-1)}
+        print(map)
+        res = []
+        for k, v in map.items(): 
+            while num >= k: 
+                res.append(v)
+                num -= k
+        
+        return ''.join(res)
+            
